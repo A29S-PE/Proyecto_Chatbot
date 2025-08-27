55808b8b8223f58c5d0970a6e6cbd4bbb08ac10b
A29S-PE <anto2011s@hotmail.com>
2025-08-27 16:52:49 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
-LLM_MODEL = "google/gemma-2b-it"
+# Configuración de LLM
+LLM_MODEL = "Qwen/Qwen2.5-1.5B-Instruct"
+LOAD_IN_4BIT = True
+LOAD_IN_8BIT = False
 
 # Configuración de clasificadores
-EMOTION_MODEL = "pysentimiento/roberta-es-sentiment"
-MENTAL_MODEL = "pysentimiento/roberta-es-sentiment"
+EMOTION_MODEL = "bert_emociones.h5"
+MENTAL_MODEL = "bert_estado_mental.h5"
+TOKENIZER_MODEL = "dccuchile/bert-base-spanish-wwm-uncased"
 
 # URL de RASA
-
 RASA_URL = "http://localhost:5005/model/parse"
-
