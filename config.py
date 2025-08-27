a530bb7c3228f4269f591e8d0381ec2126e007f4
A29S-PE <anto2011s@hotmail.com>
2025-08-27 01:54:54 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
-LLM_MODEL = "Qwen/Qwen2.5-1.5B-Instruct"
-LOAD_IN_4BIT = True
-LOAD_IN_8BIT = False
+LLM_MODEL = "google/gemma-2b-it
 
 # Configuración de clasificadores
 EMOTION_MODEL = "pysentimiento/roberta-es-sentiment"
 MENTAL_MODEL = "pysentimiento/roberta-es-sentiment"
 
 # URL de RASA
+
 RASA_URL = "http://localhost:5005/model/parse"
