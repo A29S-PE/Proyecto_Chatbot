e83c279428c20b4517df7aedec38754815869843
A29S-PE <anto2011s@hotmail.com>
2025-08-27 01:55:54 -0500

    Update llm.py

--- models/llm.py
+++ models/llm.py
@@ -1,16 +1,23 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, BitsAndBytesConfig
 from langchain.llms import HuggingFacePipeline
 from config import LLM_MODEL
+import torch
 
 def load_llm():
-    print(f"Cargando modelo {LLM_MODEL}...")
-
     tokenizer = AutoTokenizer.from_pretrained(LLM_MODEL)
+
+    quant_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4"
+    )
 
     model = AutoModelForCausalLM.from_pretrained(
         LLM_MODEL,
-        device_map="auto", 
-        torch_dtype="auto",
+        device_map="auto",
+        torch_dtype=torch.float16,
+        quantization_config=quant_config,
         trust_remote_code=True
     )
 
