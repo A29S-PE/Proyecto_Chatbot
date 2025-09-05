2c62fb1af6ae0251c7b76ebca1de4a708fc80fe1
A29S-PE <anto2011s@hotmail.com>
2025-09-05 04:02:08 -0500

    Update llm.py

--- models/llm.py
+++ models/llm.py
@@ -1,19 +1,14 @@
-from transformers import pipeline, BitsAndBytesConfig
+from transformers import pipeline
 from config import LLM_MODEL
 import torch
 
 def load_llm():
     print(f"Cargando modelo {LLM_MODEL}...")
-    quant_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_compute_dtype=torch.float16,
-        bnb_4bit_quant_type="nf4",
-    )
+
     pipe = pipeline(
         "text-generation",
         model=LLM_MODEL,
-        quantization_config=quant_config,
+        torch_dtype=torch.bfloat16,
         device_map="auto"
     )
 
