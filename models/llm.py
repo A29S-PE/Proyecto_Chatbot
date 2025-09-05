be4c6cfdf6e9b7f7f26bd92c340807e68b2e55e3
A29S-PE <anto2011s@hotmail.com>
2025-09-05 03:57:11 -0500

    Update llm.py

--- models/llm.py
+++ models/llm.py
@@ -1,27 +1,20 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
-import torch
+from transformers import pipeline, BitsAndBytesConfig
 from config import LLM_MODEL
+import torch
 
 def load_llm():
     print(f"Cargando modelo {LLM_MODEL}...")
-
     quant_config = BitsAndBytesConfig(
         load_in_4bit=True,
         bnb_4bit_use_double_quant=True,
         bnb_4bit_compute_dtype=torch.float16,
         bnb_4bit_quant_type="nf4",
     )
-
-    model = AutoModelForCausalLM.from_pretrained(
-        LLM_MODEL,
-        device_map="auto",
+    pipe = pipeline(
+        "text-generation",
+        model=LLM_MODEL,
         quantization_config=quant_config,
-        trust_remote_code=True
+        device_map="auto"
     )
 
-    tokenizer = AutoTokenizer.from_pretrained(
-        LLM_MODEL,
-        trust_remote_code=True
-    )
-
-    return tokenizer, model
+    return pipe
