d1c02b74a05d5af0f2cabac3a7abafb1e948c21c
A29S-PE <anto2011s@hotmail.com>
2025-09-04 20:22:13 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,10 @@
 LLM_MODEL = "mistralai/Mistral-7B-Instruct-v0.3"
 
 # Configuración de clasificadores
-EMOTION_MODEL = "/content/drive/MyDrive/models/bert_emociones"
-MENTAL_MODEL = "/content/drive/MyDrive/models/bert_estado_mental"
+EMOTION_MODEL = "/content/bert_emociones"
+MENTAL_MODEL = "/content/bert_estado_mental"
 TOKENIZER_MODEL = "dccuchile/bert-base-spanish-wwm-uncased"
 
 # URL de RASA
+
 RASA_URL = "http://localhost:5005/model/parse"
